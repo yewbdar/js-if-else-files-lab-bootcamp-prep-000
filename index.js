19e94acabda6a5f9f90cd86b9cea8fe917154995
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-06-24 18:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,4 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-if(name === 'Susan'){
-  console.log('the name is correct')
-}else{
-  console.log("Expected the name to be Susan")
-}
 
